e933276835f994bada76450a16c6218a24fc42fb
abdujs <abdu12.aau@gmail.com>
2025-08-03 16:52:13 +0300

    add user model with schema and interface

---
+++ src/models/user.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    role: "admin" | "author" | "reader";
+}
+
+const UserSchema: Schema<IUser> = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    role: { type: String, enum: ["admin", "author", "reader"], default: "reader" }
+}, { timestamps: true });
+
+const UserModel: Model<IUser> =
+    mongoose.models.User ||
+    mongoose.model<IUser>("User", UserSchema);
+
+export default UserModel;
